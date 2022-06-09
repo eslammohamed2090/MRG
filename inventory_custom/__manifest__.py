2390beeb184c6cd85394327f78f77d4919f97fb9
eslam <eslamkoko5541@gmail.com>
2022-06-09 11:27:10 +0200

    update2

--- inventory_custom/__manifest__.py
+++ inventory_custom/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock','account'],
+    'depends': ['base','stock','account', 'purchase'],
 
     # always loaded
     'data': [
--- inventory_custom/models/stock_picking.py
+++ inventory_custom/models/stock_picking.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 #
-from odoo import models, fields, api,_
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-#
+
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-
 
     def button_validate(self):
         res = super(StockPicking, self).button_validate()
         self.change_secondary_quantity()
+        for x in self.move_ids_without_package:
+            # avail_qty = self.env['stock.quant']._get_available_quantity(x.product_id, x.location_dest_id)
+            qty = x.product_id.qty_available
+            if not qty:
+                x.quantity_2 = 0
+                x.product_id.secondary_quantity = 0
         return res
 
     def change_secondary_quantity(self):
@@ -22,24 +28,18 @@ class StockPicking(models.Model):
                 else:
                     continue
 
-        # @api.depend('quantity_done')
-        # def get_test_uom(self):
-        #     if self.quantity_done ==0 :
-        #         quantity_2=0
-        #
-
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    secondary_uom_id = fields.Many2one('uom.uom',string='Secondary Unit Of Measure')
+    secondary_uom_id = fields.Many2one('uom.uom', string='Secondary Unit Of Measure')
     secondary_quantity = fields.Float('Secondary Quantity', )
 
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    secondary_uom_id = fields.Many2one('uom.uom',string='Secondary Unit Of Measure')
+    secondary_uom_id = fields.Many2one('uom.uom', string='Secondary Unit Of Measure')
     quantity_2 = fields.Float('Quantity Done 2')
 
     @api.onchange('product_id')
